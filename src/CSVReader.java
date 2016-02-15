ce41fc7496aeef07a9d226b5b2730ff329cad522
Drew Rapoza <arapoza@deerfield.edu>
2016-02-15 10:34:03 -0500

    print out description of user inputted character

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -5,67 +5,71 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class CSVReader {
-	
-  public static void main(String[] args) {
 
-	CSVReader obj = new CSVReader();
-	String hexValue = obj.convertToHex();
-	obj.run(hexValue);
+	public static void main(String[] args) {
 
-  }
+		CSVReader obj = new CSVReader();
+		String hexValue = obj.convertToHex();
+		obj.run(hexValue);
 
-  public void run(String hexValue) {
-	String csvFile = "/Users/araposa/Downloads/entityfacts.csv";
-	BufferedReader br = null;
-	String line = "";
-	String cvsSplitBy = ",";
-	
-	try {
+	}
 
-		br = new BufferedReader(new FileReader(csvFile));
-		while ((line = br.readLine()) != null) {
+	public void run(String hexValue) {
+		String csvFile = "/Users/araposa/Downloads/entityfacts.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
 
-		        // use comma as separator
-			String[] character = line.split(cvsSplitBy);
-			
-			
-			for ( int i=0; i<character.length ; i++ )
-		    {
-			if(character.length > 1){
-				character[1] = character[1].replaceAll("\\s","");
-				if(hexValue.equals(character[1])){
-					System.out.println(character[4]);
+		try {
+
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) 
+			{
+
+				// use comma as separator
+				String[] character = line.split(cvsSplitBy);
+
+
+				for ( int i=0; i<character.length ; i++ )
+				{
+
+					character[i] = character[i].replaceAll("\\s","");
+
+
+				}
+				if(character.length > 1)
+				{
+					if(hexValue.equals(character[1]))
+					{
+						System.out.println(character[4]);	
+					}
+				}
+			}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (br != null) {
+					try {
+						br.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 
-		    }
+			System.out.println("Done");
 		}
 
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	} finally {
-		if (br != null) {
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		public String convertToHex(){
+			Scanner input = new Scanner(System.in);
+			String s = input.nextLine();
+			int decValue = s.codePointAt(0);
+			input.close();
+
+			String hexValue = Integer.toHexString(decValue);
+			hexValue = ("0x" + hexValue);
+			return hexValue;
 		}
 	}
-
-	System.out.println("Done");
-  }
-
-  public String convertToHex(){
-		Scanner input = new Scanner(System.in);
-		String s = input.nextLine();
-		int decValue = s.codePointAt(0);
-		input.close();
-		
-		String hexValue = Integer.toHexString(decValue);
-		hexValue = ("0x" + hexValue);
-		return hexValue;
-  }
-}
