0edc859f8d448af223c23b4902598bc79f35af8d
Drew Rapoza <arapoza@deerfield.edu>
2016-02-23 17:02:45 -0500

    Revert "Delete CSVReader.java"
    This reverts commit 58ab0b9798e6a8f2a18e6202b7191b374ae3ad03.

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -5,12 +5,18 @@ public class CSVReader
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
+<<<<<<< HEAD
 		//creates new window
 		DisplayWindow d = new DisplayWindow();
 	    //creates new IdentifierLayout2 object
 		IdentifierLayout2 p = new IdentifierLayout2();
 	    //adds the IdentifierLayout2 object to the panel and then shows the frame
 		d.addPanel(p);
+=======
+		DisplayWindow d = new DisplayWindow();
+	    IdentifierLayout2 p = new IdentifierLayout2();
+	    d.addPanel(p);
+>>>>>>> parent of 58ab0b9... Delete CSVReader.java
 	    d.showFrame();
 		
 	}
